d2bba5189b82e8ad432a2b99f0276c4fdff704e1
NikolG <greplova.nikol@gmail.com>
2026-06-28 22:05:18 +0200

    cleaning script wip + test output

---
+++ scripts/clean.py
@@ -0,0 +1,28 @@
+from bookstats.formatting import clean_whitespace, clean_headers
+from bookstats.config import FILTERED_DATA, CLEAN_DATA
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(FILTERED_DATA)
+
+# Clean headers:
+clean_headers(df)
+
+# Clean whitespace on string columns:
+df["authors"] = clean_whitespace(df["authors"])
+df["title"] = clean_whitespace(df["title"])
+df["publisher"] = clean_whitespace(df["publisher"])
+
+# Formatting fixes on string columns:
+df["title"] = df["title"].str.replace(" : ", ": ")
+
+# Replace 0 to NaN on page count:
+df["num_pages"] = df["num_pages"].replace(0, np.nan)
+
+# 0 to NaN on ratings_count with non-zero average:
+has_unrated_avg = (df["average_rating"] != 0) & (df["ratings_count"] == 0)
+df["ratings_count"] = df["ratings_count"].mask(has_unrated_avg)
+
+df.to_csv(CLEAN_DATA, index=False)
+
+
