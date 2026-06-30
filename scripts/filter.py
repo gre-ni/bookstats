d43e6f9722f02e69fae97db04f57f7bc5d02b23d
NikolG <greplova.nikol@gmail.com>
2026-06-30 21:44:51 +0200

    filtering book sets

--- scripts/filter.py
+++ scripts/filter.py
@@ -7,11 +7,12 @@ df = pd.read_csv(RAW_DATA, on_bad_lines='warn')
 # Masks
 is_language = df["language_code"].isin(SUPPORTED_LANGUAGE_CODES)
 is_book = df["authors"] != "NOT A BOOK"
+is_not_set = ~df["title"].str.contains(r"#\d-\d", regex=True)
 
 # Filter by language:
 df = df[is_language]
 df = df[is_book]
-
+df = df[is_not_set]
 
 
 df.to_csv(FILTERED_DATA, index=False)
